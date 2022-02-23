a75eb1bbdb495e6403e054589dd4947bc812fbe5
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2022-02-24 00:22:28 +0600

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+var Tick = /** @class */ (function () {
+    function Tick(s, m, h) {
+        var _this = this;
+        this.tick = function () {
+            var date = new Date();
+            var sec = ((date.getSeconds() + .001) / 60) * 360 + 90;
+            var min = ((date.getMinutes() + 0.001) / 60) * 360 + 90;
+            var hour = ((date.getHours() + .001) / 24) * 360 + 90;
+            _this._sec.style.transform = "rotate(".concat(sec, "deg)");
+            _this._min.style.transform = "rotate(".concat(min, "deg)");
+            _this._hour.style.transform = "rotate(".concat(hour, "deg)");
+        };
+        this._sec = document.getElementById(s);
+        this._min = document.getElementById(m);
+        this._hour = document.getElementById(h);
+        setInterval(this.tick, 1000);
+    }
+    return Tick;
+}());
+var tick = new Tick("sec-hand", "min-hand", "hour-hand");
+//# sourceMappingURL=index.js.map
---
+++ src/ts/index.ts
@@ -0,0 +1,26 @@
+class Tick {
+    _sec: HTMLElement;
+    _min: HTMLElement;
+    _hour: HTMLElement;
+
+    constructor(s: string, m: string, h: string) {
+        this._sec = document.getElementById(s)
+        this._min = document.getElementById(m)
+        this._hour = document.getElementById(h)
+        setInterval(this.tick, 1000)
+    }
+
+    tick = () => {
+        let date = new Date();
+        let sec = ((date.getSeconds() + .001) / 60) * 360 + 90
+        let min = ((date.getMinutes() + 0.001) / 60) * 360 + 90
+        let hour = ((date.getHours() + .001) / 24) * 360 + 90
+        this._sec.style.transform = `rotate(${sec}deg)`
+        this._min.style.transform = `rotate(${min}deg)`
+        this._hour.style.transform = `rotate(${hour}deg)`
+    }
+}
+
+let tick = new Tick("sec-hand", "min-hand", "hour-hand")
+
+
